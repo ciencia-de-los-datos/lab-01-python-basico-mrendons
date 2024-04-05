08fb5329c3f53e209f256660bd91254730b55840
mrendons <160273634+mrendons@users.noreply.github.com>
2024-04-05 11:53:15 -0500

    intento2

--- preguntas.py
+++ preguntas.py
@@ -11,9 +11,18 @@ Utilice el archivo `data.csv` para resolver las preguntas.
 
 
 """
+input_file = 'data.csv'
 
+# Función para leer el archivo CSV y convertirlo en una matriz
+def csv_matriz(archivo_csv):
+    matriz = []
+    with open(input_file, newline='') as csvfile:
+        for linea in csvfile:
+            fila = linea.strip().replace('\t', ' ').split(' ')  # Dividir la línea en elementos por el espacio
+            matriz.append(fila)
+    return matriz
 
-def pregunta_01():
+def pregunta_01(sequence):
     """
     Retorne la suma de la segunda columna.
 
@@ -21,10 +30,14 @@ def pregunta_01():
     214
 
     """
-    return
+    sum = 0
+    for i in sequence:
+        sum += int(i[1])
+    return sum
 
 
-def pregunta_02():
+
+def pregunta_02(sequence):
     """
     Retorne la cantidad de registros por cada letra de la primera columna como la lista
     de tuplas (letra, cantidad), ordendas alfabéticamente.
@@ -39,10 +52,28 @@ def pregunta_02():
     ]
 
     """
-    return
+    new_sequence = []
+    for i in sequence:
+        new_sequence.append((i[0], 1))
+
+    new_sequence = sorted(new_sequence, key=lambda x: x[0])#ordena alfabeticamente
+    
+    dic={}
+    for key, value in new_sequence:
+        if key not in dic.keys():
+            dic[key] = []
+        dic[key].append(value)
+    
+    new_sequence = []  
+    for key, value in dic.items():
+        tupla = (key, sum(value))
+        new_sequence.append(tupla)
+        
+    return new_sequence
 
 
-def pregunta_03():
+
+def pregunta_03(sequence):
     """
     Retorne la suma de la columna 2 por cada letra de la primera columna como una lista
     de tuplas (letra, suma) ordendas alfabeticamente.
@@ -57,10 +88,25 @@ def pregunta_03():
     ]
 
     """
-    return
+    new_sequence = []
+    for i in sequence:
+        new_sequence.append((i[0], int(i[1])))
+    new_sequence = sorted(new_sequence, key=lambda x: x[0])#ordena alfabeticamente
+    
+    dic={}
+    for key, value in new_sequence:
+        if key not in dic.keys():
+            dic[key] = []
+        dic[key].append(value)
+    
+    new_sequence = []  
+    for key, value in dic.items():
+        tupla = (key, sum(value))
+        new_sequence.append(tupla)
+    return new_sequence
 
 
-def pregunta_04():
+def pregunta_04(sequence):
     """
     La columna 3 contiene una fecha en formato `YYYY-MM-DD`. Retorne la cantidad de
     registros por cada mes, tal como se muestra a continuación.
@@ -82,10 +128,27 @@ def pregunta_04():
     ]
 
     """
-    return
+    new_sequence = []
+    for i in sequence:
+        i[2] = i[2].split("-")
+        new_sequence.append((i[2][1],1))
+    new_sequence = sorted(new_sequence, key=lambda x: x[0])#ordena 
+
+    dic={}
+    for key, value in new_sequence:
+        if key not in dic.keys():
+            dic[key] = []
+        dic[key].append(value)
+    
+    new_sequence = []  
+    for key, value in dic.items():
+        tupla = (key, sum(value))
+        new_sequence.append(tupla)
+    return new_sequence
 
 
-def pregunta_05():
+
+def pregunta_05(sequence):
     """
     Retorne una lista de tuplas con el valor maximo y minimo de la columna 2 por cada
     letra de la columa 1.
@@ -100,10 +163,25 @@ def pregunta_05():
     ]
 
     """
-    return
+    new_sequence = []
+    for i in sequence:
+        new_sequence.append((i[0],int(i[1])))
+    new_sequence = sorted(new_sequence, key=lambda x: x[0])#ordena alfabeticamente
+
+    dic={}
+    for key, value in new_sequence:
+        if key not in dic.keys():
+            dic[key] = []
+        dic[key].append(value)
+        
+    new_sequence = []  
+    for key, value in dic.items():
+        tupla = (key, max(value), min (value))
+        new_sequence.append(tupla)
+    return new_sequence
 
 
-def pregunta_06():
+def pregunta_06(sequence):
     """
     La columna 5 codifica un diccionario donde cada cadena de tres letras corresponde a
     una clave y el valor despues del caracter `:` corresponde al valor asociado a la
@@ -125,10 +203,29 @@ def pregunta_06():
     ]
 
     """
-    return
+    new_sequence = []
+    for i in sequence:
+        i[4] = i[4].split(",")
+        for j in range(len(i[4])):
+            new_sequence.append((i[4][j].split(":")))
+            new_sequence[-1][-1]=int(new_sequence[-1][-1])
+    new_sequence = sorted(new_sequence, key=lambda x: x[0])#ordena 
+    
+    dic={}
+    for key, value in new_sequence:
+        if key not in dic.keys():
+            dic[key] = []
+        dic[key].append(value)
+    
+
+    new_sequence = []  
+    for key, value in dic.items():
+        tupla = (key, min(value), max(value))
+        new_sequence.append(tupla)
+    return new_sequence
 
 
-def pregunta_07():
+def pregunta_07(sequence):
     """
     Retorne una lista de tuplas que asocien las columnas 0 y 1. Cada tupla contiene un
     valor posible de la columna 2 y una lista con todas las letras asociadas (columna 1)
@@ -149,10 +246,25 @@ def pregunta_07():
     ]
 
     """
-    return
+    new_sequence = []
+    for i in sequence:
+        new_sequence.append((int(i[1]), i[0]))
+    new_sequence = sorted(new_sequence, key=lambda x: x[0])#ordena alfabeticamente
+    
+
+    dic={}
+    for key, value in new_sequence:
+        if key not in dic.keys():
+            dic[key] = []
+        dic[key].append(value)
+    new_sequence = []  
+    for key, value in dic.items():
+        tupla = (key, (value))
+        new_sequence.append(tupla)
+    return new_sequence
 
 
-def pregunta_08():
+def pregunta_08(sequence):
     """
     Genere una lista de tuplas, donde el primer elemento de cada tupla contiene  el valor
     de la segunda columna; la segunda parte de la tupla es una lista con las letras
@@ -174,10 +286,26 @@ def pregunta_08():
     ]
 
     """
-    return
+    new_sequence = []
+    for i in sequence:
+        new_sequence.append((int(i[1]), i[0]))
+    new_sequence = sorted(new_sequence, key=lambda x: x[0])#ordena alfabeticamente
+    
+    dic={}
+    for key, value in new_sequence:
+        if key not in dic.keys():
+            dic[key] = []
+        if value not in dic[key]:
+            dic[key].append(value)
+    new_sequence = [] 
+    
+    for key, value in dic.items():
+        tupla = (key, sorted(value))
+        new_sequence.append(tupla)
+    return new_sequence
 
 
-def pregunta_09():
+def pregunta_09(sequence):
     """
     Retorne un diccionario que contenga la cantidad de registros en que aparece cada
     clave de la columna 5.
@@ -197,10 +325,26 @@ def pregunta_09():
     }
 
     """
-    return
+    new_sequence = []
+    for i in sequence:
+        i[4] = i[4].split(",")
+        for j in range(len(i[4])):
+            new_sequence.append((i[4][j].split(":")))
+            new_sequence[-1][-1] = 1
+    new_sequence = sorted(new_sequence, key=lambda x: x[0])#ordena 
+    
 
+    new_sequence = sorted(new_sequence, key=lambda x: x[0])#ordena alfabeticamente
+    
+    dic={}
+    for key, value in new_sequence:
+        if key not in dic.keys():
+            dic[key] = 0
+        dic[key] += value
+    new_sequence = []   
+    return dic
 
-def pregunta_10():
+def pregunta_10(sequence):
     """
     Retorne una lista de tuplas contengan por cada tupla, la letra de la columna 1 y la
     cantidad de elementos de las columnas 4 y 5.
@@ -218,10 +362,14 @@ def pregunta_10():
 
 
     """
-    return
+    new_sequence = []
+    for i in sequence:
+        i[3] = i[3].split(",")
+        i[4] = i[4].split(",")
+        new_sequence.append(( i[0], len(i[3]),len(i[4])))
+    return new_sequence
 
-
-def pregunta_11():
+def pregunta_11(sequence):
     """
     Retorne un diccionario que contengan la suma de la columna 2 para cada letra de la
     columna 4, ordenadas alfabeticamente.
@@ -239,10 +387,23 @@ def pregunta_11():
 
 
     """
-    return
+    new_sequence = []
+    for i in sequence:
+        i[3] = i[3].split(",")
+        for j in i[3]:
+            new_sequence.append((j, int(i[1]) ))
+    new_sequence = sorted(new_sequence, key=lambda x: x[0])
 
+    dic={}
+    for key, value in new_sequence:
+        if key not in dic.keys():
+            dic[key] = 0
+        dic[key] += value
+    new_sequence = []   
+    return dic
+    
 
-def pregunta_12():
+def pregunta_12(sequence):
     """
     Genere un diccionario que contengan como clave la columna 1 y como valor la suma de
     los valores de la columna 5 sobre todo el archivo.
@@ -257,4 +418,20 @@ def pregunta_12():
     }
 
     """
-    return
+    new_sequence = []
+    for i in sequence:
+        i[4] = i[4].split(",")
+        for k in i[4]:
+            k = k.split(":")
+            new_sequence.append((i[0], int(k[-1]))) #para tener solo los números y unirlos con la columna 1
+    new_sequence = sorted(new_sequence, key=lambda x: x[0])
+    dic={}
+    for key, value in new_sequence:
+        if key not in dic.keys():
+            dic[key] = 0
+        dic[key] += value
+    new_sequence = []   
+    return dic
+sequence=csv_matriz(input_file)
+pregunta12 = pregunta_12(sequence)
+print(pregunta12)
